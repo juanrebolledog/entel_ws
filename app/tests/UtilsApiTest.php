e9c2b0e5d98bfe76c984c48dee5791bf1913a191
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2013-11-19 01:04:36 -0430

    Shady distance calculation testing

--- app/tests/UtilsApiTest.php
+++ app/tests/UtilsApiTest.php
@@ -2,6 +2,17 @@
 class UtilsApiTest extends TestCase {
     public function testCalculateDistance()
     {
-
+        $origin = array(
+            'lat' => 10.1010,
+            'lng' => -69.1234
+        );
+        $dest = array(
+            'lat' => 10.2020,
+            'lng' => -69.2468
+        );
+        $expected = 17556.1632205;
+        $distance = LocationModel::calculateDistance($origin, $dest);
+        $this->assertTrue(is_float($distance));
+        $this->assertTrue(round($expected) == round($distance));
     }
 } 
