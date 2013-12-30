aa183e852eaaadd17803b9ef82a0b187bc85d3c6
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2013-12-30 14:48:12 -0430

    Prueba de rutas de nivel de usuario

--- app/models/UserLevel.php
+++ app/models/UserLevel.php
@@ -2,4 +2,29 @@
 class UserLevel extends BaseModel {
     protected $table = 'usuarios_niveles';
     public $timestamps = false;
+
+    static public function getLevels()
+    {
+        return self::all()->each(function($level)
+        {
+            $level->prepareForWS();
+        });
+    }
+
+    static public function getLevel($id)
+    {
+        $level = self::find($id);
+        if ($level && $level->exists)
+        {
+            $level->prepareForWS();
+            return $level;
+        }
+        return false;
+    }
+
+    public function prepareForWS()
+    {
+        $this->imagen_on = asset($this->imagen_on);
+        $this->imagen_off = asset($this->imagen_off);
+    }
 } 
---
+++ app/tests/UserLevelsApiTest.php
@@ -0,0 +1,60 @@
+<?php
+class UserLevelsApiTest extends TestCase {
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testUserLevelIndex()
+    {
+        $resp = $this->request('GET', '/api/user_levels');
+        $content = json_decode($resp->getContent());
+        $this->assertTrue(!empty($content->data));
+        $this->assertTrue($content->status);
+    }
+
+    public function testUserLevelIndexResourcesAreUrls()
+    {
+        $resp = $this->request('GET', '/api/user_levels');
+        $content = json_decode($resp->getContent());
+        $this->assertTrue(!empty($content->data));
+        $this->assertTrue($content->status);
+        
+        foreach ($content->data as $level)
+        {
+            $this->assertEquals(1, preg_match('/^(http|https)*/', $level->imagen_on));
+            $this->assertEquals(1, preg_match('/^(http|https)*/', $level->imagen_off));
+        }
+    }
+
+    public function testUserLevelDetail()
+    {
+        $level = UserLevel::first();
+        $resp = $this->request('GET', '/api/user_levels/' . $level->id);
+        $content = json_decode($resp->getContent());
+        $this->assertTrue(!empty($content->data));
+        $this->assertTrue($content->status);
+    }
+
+    public function testUserLevelDetailResourcesAreUrls()
+    {
+        $level = UserLevel::first();
+        $resp = $this->request('GET', '/api/user_levels/' . $level->id);
+        $content = json_decode($resp->getContent());
+        $this->assertTrue(!empty($content->data));
+        $this->assertTrue($content->status);
+        $level = $content->data;
+        $this->assertEquals(1, preg_match('/^(http|https)*/', $level->imagen_on));
+        $this->assertEquals(1, preg_match('/^(http|https)*/', $level->imagen_off));
+    }
+
+    public function testUserLevelDetailUnknown()
+    {
+        $resp = $this->request('GET', '/api/user_levels/9001');
+        $content = json_decode($resp->getContent());
+        $this->assertTrue(empty($content->data));
+        $this->assertFalse($content->status);
+    }
+
+}
