ace8f3f280ebed9ea42b43866571297ddfe9e109
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2013-12-30 14:04:45 -0430

    DatabaseSeeder instala todos los seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -3,7 +3,12 @@ class DatabaseSeeder extends Seeder {
 
     public function run()
     {
+        Eloquent::unguard();
         $this->call('CategorySeeder');
+        $this->call('UserLevelSeeder');
+        $this->call('BenefitSeeder');
+        $this->call('EventSeeder');
+        $this->call('UserSeeder');
     }
 
 }
