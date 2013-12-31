7e92366d4c0349df666ad7e1e31303db4dea053c
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2013-12-31 18:37:29 -0430

    Estilos de login actualizados

--- app/views/login_layout.blade.php
+++ app/views/login_layout.blade.php
@@ -6,47 +6,9 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
     <title>Zona Entel Admin Interface</title>
     <link rel="stylesheet" href="<?= asset('css/app.css'); ?>">
+    <link rel="stylesheet" href="<?= asset('css/login.css'); ?>">
     <link rel="stylesheet" href="<?= asset('js/vendor/mapbox.js/theme/style.css'); ?>"/>
     <link href="<?= asset('css/vendor/font-awesome.css'); ?>" rel="stylesheet">
-    <style>
-        #app-content {
-            width: 30%;
-            margin: 0 auto;
-            float: none;
-        }
-        #login-form {
-            border: 1px solid #333333;
-        }
-        #login-form-header {
-            margin: 0;
-            padding: 20px;
-            padding-bottom: 5px;
-            padding-top: 10px;
-            background-color: #333333;
-            color: #FFFFFF;
-        }
-        #login-form-header h3 {
-            color: #FFFFFF;
-            margin: 0;
-        }
-        #login-form-content {
-            padding: 20px;
-            margin: 0;
-        }
-        #login-form-content .button {
-            margin-bottom: 0;
-        }
-        @media (max-width: 800px) {
-            #app-content {
-                width: 80%;
-            }
-        }
-        @media (max-width: 600px) {
-            #app-content {
-                width: 100%;
-            }
-        }
-    </style>
 </head>
 
 <body>
