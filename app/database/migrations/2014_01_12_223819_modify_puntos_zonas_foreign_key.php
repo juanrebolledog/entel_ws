46f013bd2a5db994ce915b422fce0c4372070a0a
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2014-01-13 10:40:58 -0430

    Migration actualizada

--- app/database/migrations/2014_01_12_223819_modify_puntos_zonas_foreign_key.php
+++ app/database/migrations/2014_01_12_223819_modify_puntos_zonas_foreign_key.php
@@ -31,16 +31,7 @@ class ModifyPuntosZonasForeignKey extends Migration {
      */
     public function down()
     {
-        Schema::table('puntos_zonas', function(Blueprint $table)
-        {
-            $table->dropForeign('puntos_zonas_categoria_id_foreign');
-        });
 
-        Schema::table('puntos_zonas', function(Blueprint $table)
-        {
-            $table->foreign('categoria_id')->on('puntos_zonas_categorias')->references('id')
-                ->onUpdate('cascade')->onDelete('cascade');
-        });
     }
 
 }
